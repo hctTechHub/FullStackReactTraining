a9cec2a92ce531b017eb77862258823249584392
hctTechHub <engrtemidayo2017@gmail.com>
2026-06-25 17:16:13 +0100

    Added start script to the package.json file

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import notesObj from './db.js';
+import 'dotenv/config' // require('dotenv').config()
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.static('dist'));
 app.use(cors());
